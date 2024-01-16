2afbc19a5b7c9c9bb1214a412155e7c2b2170cc5
Darina Kuznetsova <holtzdari@gmail.com>
2024-01-16 13:15:36 +0300

    Updated order of comments, comments date format

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments
+  has_many :comments,  -> { order(created_at: :desc) }, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
